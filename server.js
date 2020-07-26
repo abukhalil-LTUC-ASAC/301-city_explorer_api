475a5cc3ce90342681288b1f5c728799837d4ac3
Abukhalil95 <yahya_abukhaleel@hotmail.com>
2020-07-26 14:14:54 +0300

    added missing initial files

--- server.js
+++ server.js
@@ -1,4 +1,31 @@
-var express = require('express')
-var app = express()
 
-require('dotenv').config()
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./public'));
+
+// app.get('/data', (request, response) => {
+//   let airplanes = {
+//     departure: Date.now(),
+//     canFly: true,
+//     pilot: 'Well Trained',
+//   };
+//   response.status(200).json(airplanes);
+// });
+
+
+app.get('/', function (req, res) {
+  res.status(200).send('Hello World!');
+});
+
+
+
+app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
+
+app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
+
